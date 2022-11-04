d2cfa05b48a8b033ff7799c5fd5ee03d187622aa
Gabe Cook <gabe565@gmail.com>
2022-11-04 15:13:03 -0500

    :bulb: Fix processor status godoc

--- internal/cpu/flags.go
+++ internal/cpu/flags.go
@@ -2,15 +2,17 @@ package cpu
 
 import "github.com/gabe565/gones/internal/bits"
 
-// .----------------- Negative Flag
-// | .--------------- Overflow Flag
-// | |   .----------- Break Command
-// | |   | .--------- Decimal Mode (not used on NES)
-// | |   | | .------- Interrupt Disable
-// | |   | | | .----- Zero Flag
-// | |   | | | | .--- Carry Flag
-// N V _ B D I Z C
-// 7 6 5 4 3 2 1 0
+// Processor Status bits
+//
+//	.----------------- Negative Flag
+//	| .--------------- Overflow Flag
+//	| |   .----------- Break Command
+//	| |   | .--------- Decimal Mode (not used on NES)
+//	| |   | | .------- Interrupt Disable
+//	| |   | | | .----- Zero Flag
+//	| |   | | | | .--- Carry Flag
+//	N V _ B D I Z C
+//	7 6 5 4 3 2 1 0
 const (
 	Carry bits.Bits = 1 << iota
 	Zero
