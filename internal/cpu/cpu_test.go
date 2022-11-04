0d62eda11155ed5ad97ac7fc6e813c5db4fad092
Gabe Cook <gabe565@gmail.com>
2022-11-04 10:56:33 -0500

    :truck: Capitalize hex values

--- internal/cpu/cpu_test.go
+++ internal/cpu/cpu_test.go
@@ -7,7 +7,7 @@ import (
 
 func Test_0xa9_lda_immediate_load_data(t *testing.T) {
 	cpu := New()
-	if err := cpu.loadAndRun([]uint8{0xa9, 0x05, 0x00}); err != nil {
+	if err := cpu.loadAndRun([]uint8{0xA9, 0x05, 0x00}); err != nil {
 		assert.NoErrorf(t, err, "loadAndRun")
 	}
 
@@ -18,7 +18,7 @@ func Test_0xa9_lda_immediate_load_data(t *testing.T) {
 
 func Test_0xa9_lda_zero_flag(t *testing.T) {
 	cpu := New()
-	if err := cpu.loadAndRun([]uint8{0xa9, 0x00, 0x00}); err != nil {
+	if err := cpu.loadAndRun([]uint8{0xA9, 0x00, 0x00}); err != nil {
 		assert.NoErrorf(t, err, "loadAndRun")
 	}
 
@@ -28,7 +28,7 @@ func Test_0xa9_lda_zero_flag(t *testing.T) {
 func Test_0xaa_tax_move_a_to_x(t *testing.T) {
 	cpu := New()
 	cpu.RegisterA = 10
-	if err := cpu.loadAndRun([]uint8{0xa9, 0x0a, 0xaa, 0x00}); err != nil {
+	if err := cpu.loadAndRun([]uint8{0xA9, 0x0A, 0xAA, 0x00}); err != nil {
 		assert.NoErrorf(t, err, "loadAndRun")
 	}
 
@@ -37,16 +37,16 @@ func Test_0xaa_tax_move_a_to_x(t *testing.T) {
 
 func Test_5_operations(t *testing.T) {
 	cpu := New()
-	if err := cpu.loadAndRun([]uint8{0xa9, 0xc0, 0xaa, 0xe8, 0x00}); err != nil {
+	if err := cpu.loadAndRun([]uint8{0xA9, 0xC0, 0xAA, 0xE8, 0x00}); err != nil {
 		assert.NoErrorf(t, err, "loadAndRun")
 	}
 
-	assert.EqualValues(t, 0xc1, cpu.RegisterX)
+	assert.EqualValues(t, 0xC1, cpu.RegisterX)
 }
 
 func Test_inx_overflow(t *testing.T) {
 	cpu := New()
-	if err := cpu.loadAndRun([]uint8{0xa9, 0xff, 0xaa, 0xe8, 0xe8, 0x00}); err != nil {
+	if err := cpu.loadAndRun([]uint8{0xA9, 0xFF, 0xAA, 0xE8, 0xE8, 0x00}); err != nil {
 		assert.NoErrorf(t, err, "loadAndRun")
 	}
 
@@ -56,7 +56,7 @@ func Test_inx_overflow(t *testing.T) {
 func Test_lda_from_memory(t *testing.T) {
 	cpu := New()
 	cpu.memWrite(0x10, 0x55)
-	if err := cpu.loadAndRun([]uint8{0xa5, 0x10, 0x00}); err != nil {
+	if err := cpu.loadAndRun([]uint8{0xA5, 0x10, 0x00}); err != nil {
 		assert.NoErrorf(t, err, "loadAndRun")
 	}
 
