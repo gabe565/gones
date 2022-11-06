a6ae44bd0cdab95f3e0f33aaebebaf036c883082
Gabe Cook <gabe565@gmail.com>
2022-11-06 15:43:03 -0600

    :zap: Optimize frame building and drawing logic

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"image/color"
 	"math/rand"
 	"os"
-	"reflect"
 	"time"
 )
 
@@ -37,7 +36,9 @@ func run(path string) error {
 
 	rand.Seed(time.Now().UnixNano())
 
-	var lastImg image.Image
+	frameStart := 0x200
+	frameEnd := 0x600
+	frame := make([]byte, frameEnd-frameStart+1)
 	c.Callback = func(c *cpu.CPU) error {
 		if win.Pressed(pixelgl.KeyEscape) {
 			return cpu.ErrBrk
@@ -51,41 +52,47 @@ func run(path string) error {
 			c.MemWrite(0xFF, 0x64)
 		}
 
-		img := image.NewRGBA(image.Rect(0, 0, 32, 32))
-		for addr := 0x200; addr <= 0x600; addr += 1 {
-			pxl := c.MemRead(uint16(addr))
-			var c color.Color
-			switch pxl {
-			case 0:
-				c = color.Black
-			case 1:
-				c = color.White
-			case 2, 9:
-				c = color.Gray16{Y: 0x8888}
-			case 3, 10:
-				c = color.RGBA{R: 0xFF, A: 0xFF}
-			case 4, 11:
-				c = color.RGBA{G: 0xFF, A: 0xFF}
-			case 5, 12:
-				c = color.RGBA{B: 0xFF, A: 0xFF}
-			case 6, 13:
-				c = color.RGBA{R: 0xFF, B: 0xFF, A: 0xFF}
-			case 7, 14:
-				c = color.RGBA{R: 0xFF, G: 0xFF, A: 0xFF}
-			default:
-				c = color.RGBA{G: 0xFF, B: 0xFF, A: 0xFF}
+		var drawFrame bool
+		for k, prevVal := range frame {
+			val := c.MemRead(uint16(frameStart + k))
+			if !drawFrame && val != prevVal {
+				drawFrame = true
 			}
-			k := addr - 0x200
-			img.Set(k%32, k/32, c)
+			frame[k] = val
 		}
 
-		if !reflect.DeepEqual(img, lastImg) {
+		if drawFrame {
+			img := image.NewRGBA(image.Rect(0, 0, 32, 32))
+			for k, pxl := range frame {
+				var c color.Color
+				switch pxl {
+				case 0:
+					c = color.Black
+				case 1:
+					c = color.White
+				case 2, 9:
+					c = color.Gray16{Y: 0x8888}
+				case 3, 10:
+					c = color.RGBA{R: 0xFF, A: 0xFF}
+				case 4, 11:
+					c = color.RGBA{G: 0xFF, A: 0xFF}
+				case 5, 12:
+					c = color.RGBA{B: 0xFF, A: 0xFF}
+				case 6, 13:
+					c = color.RGBA{R: 0xFF, B: 0xFF, A: 0xFF}
+				case 7, 14:
+					c = color.RGBA{R: 0xFF, G: 0xFF, A: 0xFF}
+				default:
+					c = color.RGBA{G: 0xFF, B: 0xFF, A: 0xFF}
+				}
+				img.Set(k%32, k/32, c)
+			}
+
 			win.Clear(color.Black)
 			pic := pixel.PictureDataFromImage(img)
 			sprite := pixel.NewSprite(pic, pic.Bounds())
 			sprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()).Scaled(win.Bounds().Center(), 10))
 			win.Update()
-			lastImg = img
 			time.Sleep(time.Second / 60)
 		}
 
