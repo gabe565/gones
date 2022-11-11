107f02f7a1a05310177515664ffd19a953034f04
Gabe Cook <gabe565@gmail.com>
2022-11-10 22:56:09 -0600

    :recycle: Move validation to pre-run function

--- cmd.go
+++ cmd.go
@@ -9,13 +9,15 @@ import (
 func NewCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use: "gones ROM",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			var err error
-
+		PreRunE: func(cmd *cobra.Command, args []string) error {
 			if len(args) < 1 {
 				return errors.New("No ROM provided")
 			}
 			cmd.SilenceUsage = true
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var err error
 			pixelgl.Run(func() {
 				err = run(args[0])
 			})
