3755fc45b2cd044a2ba542781b2448098e31fa27
Gabe Cook <gabe565@gmail.com>
2022-11-21 16:04:11 -0600

    :recycle: (apu): Hardcode triangle outputs instead of computing

--- internal/apu/triangle.go
+++ internal/apu/triangle.go
@@ -1,5 +1,10 @@
 package apu
 
+var triangleOutputs = []byte{
+	15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+}
+
 type Triangle struct {
 	Enabled bool
 
@@ -80,10 +85,6 @@ func (t *Triangle) output() byte {
 	case t.CounterValue == 0:
 		return 0
 	default:
-		if t.DutyValue < 16 {
-			return 15 - t.DutyValue
-		} else {
-			return t.DutyValue - 16
-		}
+		return triangleOutputs[t.DutyValue]
 	}
 }
