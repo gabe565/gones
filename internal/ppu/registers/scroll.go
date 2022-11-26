4eea068bbd2a55a70df9808ade7f3ce25b49e6b9
Gabe Cook <gabe565@gmail.com>
2022-11-26 17:07:52 -0600

    :art: (ppu): Tweak scroll register syntax

--- internal/ppu/registers/scroll.go
+++ internal/ppu/registers/scroll.go
@@ -1,16 +1,15 @@
 package registers
 
 type Scroll struct {
-	X     byte
-	Y     byte
+	X, Y  byte
 	Latch bool
 }
 
 func (s *Scroll) Write(data byte) {
-	if !s.Latch {
-		s.X = data
-	} else {
+	if s.Latch {
 		s.Y = data
+	} else {
+		s.X = data
 	}
 	s.Latch = !s.Latch
 }
