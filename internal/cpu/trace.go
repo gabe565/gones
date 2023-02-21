c62440694466db8054acb104abe8fec62b1a72bd
Gabe Cook <gabe565@gmail.com>
2023-02-20 23:57:30 -0600

    :art: (trace): Combine fmt.Sprintf statements

--- internal/cpu/trace.go
+++ internal/cpu/trace.go
@@ -85,11 +85,13 @@ func (c *CPU) Trace() string {
 		hexStr += fmt.Sprintf("%02X ", v)
 	}
 	hexStr = strings.TrimSpace(hexStr)
-	asmStr := fmt.Sprintf("%04X  %-8s %4s %s", begin, hexStr, op.Mnemonic, trace)
 
 	final := fmt.Sprintf(
-		"%-47s A:%02X X:%02X Y:%02X P:%02X SP:%02X",
-		asmStr,
+		"%04X  %-8s %4s %-27s A:%02X X:%02X Y:%02X P:%02X SP:%02X",
+		begin,
+		hexStr,
+		op.Mnemonic,
+		trace,
 		c.Accumulator,
 		c.RegisterX,
 		c.RegisterY,
