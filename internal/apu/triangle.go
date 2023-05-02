5fc0064a945c523dfd1c1f8228c173dd97b50ac9
Gabe Cook <gabe565@gmail.com>
2023-05-02 00:34:14 -0500

    :bug: (apu): Fix triangle channel popping

--- internal/apu/triangle.go
+++ internal/apu/triangle.go
@@ -1,8 +1,8 @@
 package apu
 
 var triangleOutputs = [...]byte{
-	15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
 	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+	15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
 }
 
 type Triangle struct {
@@ -77,16 +77,5 @@ func (t *Triangle) stepCounter() {
 }
 
 func (t *Triangle) output() byte {
-	switch {
-	case !t.Enabled:
-		return 0
-	case t.TimerPeriod < 3:
-		return 0
-	case t.LengthValue == 0:
-		return 0
-	case t.CounterValue == 0:
-		return 0
-	default:
-		return triangleOutputs[t.DutyValue]
-	}
+	return triangleOutputs[t.DutyValue]
 }
