gabe565/gones

commit c2c01789c67cbb3b81e117587d67c1a8b6fd8d8e

chore(apu): APU sampling tweaks

internal/apu/apu.go
@@ -13,7 +13,7 @@ type CPU interface {
 	AddStall(uint8)
 }
 
-const FrameCounterRate = consts.CpuFrequency / 240.0
+const FrameCounterRate = consts.CpuFrequency / float32(240.0)
 
 var lengthTable = [...]byte{
 	10, 254, 20, 2, 40, 4, 80, 6, 160, 8, 60, 10, 14, 12, 26, 14,
@@ -31,18 +31,18 @@ const (
 func New() *APU {
 	return &APU{
 		Enabled:    true,
-		SampleRate: consts.CpuFrequency / float64(consts.AudioSampleRate),
+		SampleRate: consts.CpuFrequency / float32(consts.AudioSampleRate),
 
 		Square: [2]Square{{Channel1: true}, {}},
 		Noise:  Noise{ShiftRegister: 1},
 
-		buf: make(chan float32, 10*consts.AudioSampleRate/60),
+		buf: make(chan float32, 8*consts.AudioSampleRate/20),
 	}
 }
 
 type APU struct {
 	Enabled    bool
-	SampleRate float64
+	SampleRate float32
 	cpu        CPU
 
 	Square   [2]Square
@@ -124,20 +124,20 @@ func (a *APU) Reset() {
 }
 
 func (a *APU) Step() {
-	cycle1 := float64(a.Cycle)
+	cycle1 := float32(a.Cycle)
 	a.Cycle += 1
-	cycle2 := float64(a.Cycle)
+	cycle2 := float32(a.Cycle)
 
 	a.stepTimer()
 
-	f1 := int(cycle1 / FrameCounterRate)
-	f2 := int(cycle2 / FrameCounterRate)
+	f1 := uint32(cycle1 / FrameCounterRate)
+	f2 := uint32(cycle2 / FrameCounterRate)
 	if f1 != f2 {
 		a.stepFrameCounter()
 	}
 
-	s1 := int(cycle1 / a.SampleRate)
-	s2 := int(cycle2 / a.SampleRate)
+	s1 := uint32(cycle1 / a.SampleRate)
+	s2 := uint32(cycle2 / a.SampleRate)
 	if s1 != s2 && a.Enabled {
 		a.sendSample()
 	}

internal/consts/consts.go
@@ -10,7 +10,7 @@ const (
 	// ChrChunkSize is the size of the smallest CHR
 	ChrChunkSize = 0x2000
 
-	CpuFrequency = 1789772
+	CpuFrequency = 1789773
 
 	AudioSampleRate = 44100
 )

commit 8b022accabf42ba7101f2f90512117bb61a8eb04

fix(apu): Fix audio bug introduced by c2c01789

internal/apu/apu.go
@@ -13,7 +13,7 @@ type CPU interface {
 	interrupts.Stallable
 }
 
-const FrameCounterRate = consts.CpuFrequency / float32(240.0)
+const FrameCounterRate = consts.CpuFrequency / float64(240.0)
 
 var lengthTable = [...]byte{
 	10, 254, 20, 2, 40, 4, 80, 6, 160, 8, 60, 10, 14, 12, 26, 14,
@@ -31,7 +31,7 @@ const (
 func New() *APU {
 	return &APU{
 		Enabled:    true,
-		SampleRate: consts.CpuFrequency / float32(consts.AudioSampleRate),
+		SampleRate: consts.CpuFrequency / float64(consts.AudioSampleRate),
 
 		Square: [2]Square{{Channel1: true}, {}},
 		Noise:  Noise{ShiftRegister: 1},
@@ -42,7 +42,7 @@ func New() *APU {
 
 type APU struct {
 	Enabled    bool
-	SampleRate float32 `msgpack:"-"`
+	SampleRate float64 `msgpack:"-"`
 	cpu        CPU
 
 	Square   [2]Square
@@ -125,9 +125,9 @@ func (a *APU) Reset() {
 }
 
 func (a *APU) Step() {
-	cycle1 := float32(a.Cycle)
+	cycle1 := float64(a.Cycle)
 	a.Cycle += 1
-	cycle2 := float32(a.Cycle)
+	cycle2 := float64(a.Cycle)
 
 	a.stepTimer()
 