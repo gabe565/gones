c8bf53e2e485aa8d5775c9162238e28c59176e5b
Gabe Cook <gabe565@gmail.com>
2024-02-25 23:49:27 -0600

    chore(config): Change keymap order

--- internal/config/keymap.go
+++ internal/config/keymap.go
@@ -6,28 +6,29 @@ import (
 )
 
 type Keymap struct {
-	Up     ebiten.Key `toml:"up"`
-	Left   ebiten.Key `toml:"left"`
-	Down   ebiten.Key `toml:"down"`
-	Right  ebiten.Key `toml:"right"`
-	Start  ebiten.Key `toml:"start"`
-	Select ebiten.Key `toml:"select"`
 	A      ebiten.Key `toml:"a"`
 	B      ebiten.Key `toml:"b"`
+	Start  ebiten.Key `toml:"start"`
+	Select ebiten.Key `toml:"select"`
+	Up     ebiten.Key `toml:"up"`
+	Down   ebiten.Key `toml:"down"`
+	Left   ebiten.Key `toml:"left"`
+	Right  ebiten.Key `toml:"right"`
+
 	ATurbo ebiten.Key `toml:"a_turbo" comment:"Key to press the A button repeatedly (must be held)."`
 	BTurbo ebiten.Key `toml:"b_turbo" comment:"Key to press the B button repeatedly (must be held)."`
 }
 
 func (k Keymap) GetMap() map[button.Button]ebiten.Key {
 	return map[button.Button]ebiten.Key{
-		button.Up:     k.Up,
-		button.Left:   k.Left,
-		button.Down:   k.Down,
-		button.Right:  k.Right,
-		button.Start:  k.Start,
-		button.Select: k.Select,
 		button.A:      k.A,
 		button.B:      k.B,
+		button.Start:  k.Start,
+		button.Select: k.Select,
+		button.Up:     k.Up,
+		button.Down:   k.Down,
+		button.Left:   k.Left,
+		button.Right:  k.Right,
 	}
 }
 
