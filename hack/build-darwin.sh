4f151c663405f544ada03880438cfeff0dcb877e
Gabe Cook <gabe565@gmail.com>
2024-02-28 23:34:55 -0600

    chore(hack): Add vars for iconset/icns [skip ci]

--- hack/build-darwin.sh
+++ hack/build-darwin.sh
@@ -2,23 +2,25 @@
 
 BINARY_NAME='gones'
 APP_NAME='GoNES'
+ICONSET=GoNES.iconset
+ICNS=GoNES.icns
 
 set -euo pipefail
 
 cd "$(git rev-parse --show-toplevel)"
 
-rm -rf dist/*.app assets/GoNES.iconset
+rm -rf dist/*.app assets/{"$ICONSET","$ICNS"}
 mkdir -p dist
 
 # Generate icns
-cp -a assets/{png,GoNES.iconset}
-cp -a assets/GoNES.iconset/icon_{32x32,16x16@2x}.png
-rm assets/GoNES.iconset/icon_48x48.png
-cp -a assets/GoNES.iconset/icon_{64x64,32x32@2x}.png
-cp -a assets/GoNES.iconset/icon_{128x128,64x64@2x}.png
-cp -a assets/GoNES.iconset/icon_{256x256,128x128@2x}.png
-cp -a assets/GoNES.iconset/icon_{512x512,256x256@2x}.png
-iconutil --convert icns --output assets/GoNES.icns assets/GoNES.iconset
+cp -a assets/{png,"$ICONSET"}
+cp -a assets/"$ICONSET"/icon_{32x32,16x16@2x}.png
+rm assets/"$ICONSET"/icon_48x48.png
+cp -a assets/"$ICONSET"/icon_{64x64,32x32@2x}.png
+cp -a assets/"$ICONSET"/icon_{128x128,64x64@2x}.png
+cp -a assets/"$ICONSET"/icon_{256x256,128x128@2x}.png
+cp -a assets/"$ICONSET"/icon_{512x512,256x256@2x}.png
+iconutil --convert icns --output "assets/$ICNS" "assets/$ICONSET"
 
 # Build binaries
 export GOOS=darwin CGO_ENABLED=1
@@ -38,7 +40,7 @@ APP_CONTENTS="dist/$APP_NAME.app/Contents"
 mkdir -p "$APP_CONTENTS"
 cp "assets/info.plist" "$APP_CONTENTS"
 mkdir "$APP_CONTENTS/Resources"
-cp "assets/GoNES.icns" "$APP_CONTENTS/Resources"
+cp "assets/$ICNS" "$APP_CONTENTS/Resources"
 mkdir "$APP_CONTENTS/MacOS"
 mv "dist/$BINARY_NAME" "$APP_CONTENTS/MacOS"
 echo ...done
