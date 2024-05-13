dc110a783ed9ef914ea9c30ee25d5b778b9619ec
Gabe Cook <gabe565@gmail.com>
2024-05-12 23:59:42 -0500

    chore: Move mousetrap disable statement out of `init` func

--- cmd/gones/mousetrap_windows.go
+++
@@ -1,7 +0,0 @@
-package gones
-
-import "github.com/spf13/cobra"
-
-func init() { //nolint:all
-	cobra.MousetrapHelpText = ""
-}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gabe565/gones/cmd/gones"
 	_ "github.com/gabe565/gones/internal/pprof"
 	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
 )
 
 //go:generate cp $GOROOT/misc/wasm/wasm_exec.js web/src/scripts
@@ -15,6 +16,7 @@ const (
 )
 
 func main() {
+	cobra.MousetrapHelpText = ""
 	rootCmd := gones.New()
 	rootCmd.Version = buildVersion()
 	if err := rootCmd.Execute(); err != nil {
