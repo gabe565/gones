4f28b29c58415a64618900d6ba96ac22bcf489cf
Gabe Cook <gabe565@gmail.com>
2024-10-14 15:51:26 -0500

    feat(nesutil): Add subcommand to split CHR and PRG from a ROM file

---
+++ cmd/nesutil/ines/extract/cmd.go
@@ -0,0 +1,61 @@
+package extract
+
+import (
+	"log/slog"
+	"os"
+
+	"gabe565.com/gones/internal/cartridge"
+	"gabe565.com/gones/internal/util"
+	"github.com/spf13/cobra"
+)
+
+const (
+	FlagPRG = "prg"
+	FlagCHR = "chr"
+)
+
+func New() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "extract ROM",
+		Short: "Extract PRG/CHR ROM data from an INES ROM",
+		Args:  cobra.ExactArgs(1),
+		RunE:  run,
+
+		ValidArgsFunction: util.CompleteROM,
+	}
+
+	flag := cmd.Flags()
+	flag.StringP(FlagPRG, "p", "", "PRG ROM output file path")
+	flag.StringP(FlagCHR, "c", "", "CHR ROM output file path")
+	cmd.MarkFlagsOneRequired(FlagPRG, FlagCHR)
+
+	return cmd
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	path := args[0]
+	cart, err := cartridge.FromINESFile(path)
+	if err != nil {
+		return err
+	}
+
+	if prg := util.Must2(cmd.Flags().GetString(FlagPRG)); prg != "" {
+		slog.Info("Extracting PRG", "path", prg)
+		if err := os.WriteFile(prg, cart.PRG, 0o644); err != nil {
+			return err
+		}
+	}
+
+	if chr := util.Must2(cmd.Flags().GetString(FlagCHR)); chr != "" {
+		if cart.Header.CHRCount == 0 {
+			slog.Warn("Game does not have CHR. Skipping")
+		} else {
+			slog.Info("Extracting CHR", "path", chr)
+			if err := os.WriteFile(chr, cart.CHR, 0o644); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
--- docs/nesutil.md
+++ docs/nesutil.md
@@ -10,5 +10,6 @@ GoNES command-line utilities
 
 ### SEE ALSO
 
+* [nesutil ines](nesutil_ines.md)	 - Commands that work with INES files
 * [nesutil ls](nesutil_ls.md)	 - List ROM files and metadata
 
